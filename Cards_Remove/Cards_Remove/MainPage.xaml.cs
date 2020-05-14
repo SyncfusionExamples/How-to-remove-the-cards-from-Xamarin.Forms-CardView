3d8d688b32d1ebe8f74901e1a27ecd0da26346c7
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-05-14 12:01:16 +0530

    Null check added

--- Cards_Remove/Cards_Remove/MainPage.xaml.cs
+++ Cards_Remove/Cards_Remove/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Syncfusion.XForms.Cards;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -16,18 +17,19 @@ namespace Cards_Remove
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        ObservableCollection<CardModel> items;
+
         public MainPage()
         {
             InitializeComponent();
+            items = (BindingContext as CardViewModel)?.Items;
             On<iOS>().SetUseSafeArea(true);
         }
 
         private void SfCardView_Dismissed(object sender, DismissedEventArgs e)
         {
-            var item = (sender as SfCardView).BindingContext as CardModel;
-
-            var items = (this.BindingContext as CardViewModel).Items;
-            if (items.Contains(item))
+            var item = (sender as SfCardView)?.BindingContext as CardModel;
+            if (items != null && item != null && items.Contains(item))
             {
                 items.Remove(item);
             }
